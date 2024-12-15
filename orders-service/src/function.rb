78a574d6ae71872523aafeaf31d61a11f2a69cd4
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-12-15 23:33:59 +0100

    Update handler for API Gateway specific data

--- orders-service/src/function.rb
+++ orders-service/src/function.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 require 'securerandom'
 require 'dry-validation'
 require 'aws-sdk-dynamodb'
@@ -7,19 +9,29 @@ require 'aws-sdk-dynamodb'
 require_relative 'orders_service'
 
 module Function
-  # @param event [Hash<Object, Object>]
-  # @return [Hash<Symbol, Object>]
-  def self.perform(event:, **_) # rubocop:disable Metrics/AbcSize
-    OrdersService.logger.info(event.transform_keys(&:to_sym).inspect)
-    form = OrdersService.new_order_form(event: event.transform_keys(&:to_sym))
+  class << self
+    # @param event [Hash<Object, Object>]
+    # @return [Hash<Symbol, Object>]
+    def perform(event:, **_) # rubocop:disable Metrics/AbcSize
+      OrdersService.logger.info(event.transform_keys(&:to_sym).inspect)
+      form = OrdersService.new_order_form(event: new_order_data(event))
 
-    if form.valid?
-      OrdersService.logger.info('Creating order...')
-      order = OrdersService.repo.add(order: form.to_order)
-      { status: :success, data: { order_id: order.order_id } }
-    else
-      OrdersService.logger.info('Invalid data received...')
-      { status: :failure, data: { errors: form.errors.map(&:to_h) } }
+      if form.valid?
+        OrdersService.logger.info('Creating order...')
+        order = OrdersService.repo.add(order: form.to_order)
+        { statusCode: 200, body: JSON.generate(data: { order_id: order.order_id }) }
+      else
+        OrdersService.logger.info('Invalid data received...')
+        { statusCode: 400, body: JSON.generate(data: { errors: form.errors.map(&:to_h) }) }
+      end
+    end
+
+    private
+
+    # @param event [Hash<Object, Object>]
+    # @return [Hash<Symbol, Object>]
+    def new_order_data(event)
+      JSON.parse(event.fetch('body'), symbolize_names: true)
     end
   end
 end
--- orders-service/src/spec/function_spec.rb
+++ orders-service/src/spec/function_spec.rb
@@ -2,23 +2,24 @@
 
 RSpec.describe Function do
   let(:ctx) { {} }
+  let(:event) { { 'body' => JSON.generate(order_data) } }
 
   context 'when event contains valid data' do
-    let(:event) { { 'customer_id' => '0xFF', 'total' => 10 } }
+    let(:order_data) { { 'customer_id' => '0xFF', 'total' => 10 } }
 
     it 'responds with successs' do
       result = described_class.perform(event:, context: ctx)
-      expect(result[:status]).to eq(:success)
+      expect(result[:statusCode]).to eq(200)
     end
   end
 
   context 'when event contains invalid data' do
-    let(:event) { { 'customer_id' => '0xFF', 'total' => -5 } }
+    let(:order_data) { { 'customer_id' => '0xFF', 'total' => -5 } }
 
     it 'responds with failure' do
       result = described_class.perform(event:, context: ctx)
 
-      expect(result[:status]).to eq(:failure)
+      expect(result[:statusCode]).to eq(400)
     end
   end
 end
