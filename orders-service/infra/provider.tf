471f346c96fc52d49f0bb4681fc84f970870ebbd
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-12-15 23:43:42 +0100

    Remove hashicorp/archive terraform provider

--- orders-service/infra/provider.tf
+++ orders-service/infra/provider.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.81.0"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.7.0"
-    }
   }
 
   required_version = "~> 1.0"
